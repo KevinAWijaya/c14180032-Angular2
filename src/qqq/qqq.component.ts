5083fba30bab2bc09a07cd74222cb703c378d0ea
KevinAWijaya <kevinaw46@gmail.com>
2021-03-02 19:11:54 +0700

    3 files added

---
+++ src/qqq/qqq.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qqq',
+  templateUrl: './qqq.component.html',
+  styleUrls: ['./qqq.component.css']
+})
+export class QqqComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
